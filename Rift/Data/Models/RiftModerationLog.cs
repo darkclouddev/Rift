f82136769f20c29d2c29754b63e670a068167e28
Andrey Danilov <this@ionpri.me>
2019-06-06 19:32:58 +0300

    Added missing models

---
+++ Rift/Data/Models/RiftModerationLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rift.Data.Models
+{
+    public class RiftModerationLog
+    {
+        public uint Id { get; set; }
+        public ulong ModeratorId { get; set; }
+        public ulong TargetId { get; set; }
+        public string  Action { get; set; }
+        public string Reason { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
