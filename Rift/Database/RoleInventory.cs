9f8d66562439ce7f1567c00364dc00298aa61c08
Andrey Danilov <this@ionpri.me>
2019-06-30 20:14:58 +0300

    Added ability to add inventory roles

--- Rift/Database/RoleInventory.cs
+++ Rift/Database/RoleInventory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,23 @@ namespace Rift.Database
 {
     public class RoleInventory
     {
+        public async Task AddAsync(ulong userId, ulong roleId, string source)
+        {
+            var invRole = new RiftRoleInventory
+            {
+                UserId = userId,
+                RoleId = roleId,
+                ObtainedAt = DateTime.UtcNow,
+                ObtainedFrom = source
+            };
+            
+            using (var context = new RiftContext())
+            {
+                await context.RoleInventories.AddAsync(invRole);
+                await context.SaveChangesAsync();
+            }
+        }
+        
         public async Task<List<RiftRoleInventory>> GetAsync(ulong userId)
         {
             using (var context = new RiftContext())
