b340dc0a148af31cff98255aee2de1055a000782
Andrey Danilov <this@ionpri.me>
2019-08-16 14:45:32 +0300

    Changed giveaway name template

--- Rift/Services/Message/Templates/Giveaway/GiveawayName.cs
+++ Rift/Services/Message/Templates/Giveaway/GiveawayName.cs
@@ -4,15 +4,15 @@ using Rift.Data.Models;
 
 namespace Rift.Services.Message.Templates.Giveaway
 {
-    public class GiveawayName : TemplateBase
+    public class GiveawayDescription : TemplateBase
     {
-        public GiveawayName() : base(nameof(GiveawayName))
+        public GiveawayDescription() : base(nameof(GiveawayDescription))
         {
         }
 
         public override Task<RiftMessage> ApplyAsync(RiftMessage message, FormatData data)
         {
-            return ReplaceDataAsync(message, data.Giveaway.Stored.Name);
+            return ReplaceDataAsync(message, data.Giveaway.Stored.Description);
         }
     }
 }
